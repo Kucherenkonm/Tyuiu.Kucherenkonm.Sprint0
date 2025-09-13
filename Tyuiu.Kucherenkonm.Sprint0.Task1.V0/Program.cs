c7b1eb6543fa40a7876236d3a8488e8717c69d1e
wyrtnvll <date1818@yandex.ru>
2025-09-13 11:22:40 +0500

    Task1

--- Tyuiu.Kucherenkonm.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.Kucherenkonm.Sprint0.Task1.V0/Program.cs
@@ -1,22 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-namespace Tyuiu.Kucherenkonm.Sprint0.Task1.V0
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-            string name = "Надежда";
-            int age = 18;
-            Console.WriteLine("Здравствуйте, меня зовут " + name + " мне " + age + " лет!");
-            Console.ReadKey();
-
-        }
-    }
-}
+﻿Console.WriteLine("Hello, World!");
+string name = "Надежда";
+int age = 18;
+Console.WriteLine("Здравствуйте, меня зовут " + name + " мне " + age + " лет!");
+Console.ReadKey();
 
 
