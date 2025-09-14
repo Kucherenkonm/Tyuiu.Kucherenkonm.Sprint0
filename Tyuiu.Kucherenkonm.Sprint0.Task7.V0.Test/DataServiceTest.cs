68674acc4c76efe22ddcef8afcaa88e81697fa10
wyrtnvll <date1818@yandex.ru>
2025-09-15 00:06:05 +0500

    Task7

---
+++ Tyuiu.Kucherenkonm.Sprint0.Task7.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.Kucherenkonm.Sprint0.Task7.V0.Lib;
+namespace Tyuiu.Kucherenkonm.Sprint0.Task7.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionArraysValid()
+        {
+            int[] nums1 = { 1, 1, 1, 1, 1 };
+            int[] nums2 = { 1, 1, 1, 1, 1 };
+            int[] nums3 = { 2, 2, 2, 2, 2 };
+            int[] res = DataService.AdditionArrays(nums1, nums2);
+            CollectionAssert.AreEqual(nums3, res);
+        }
+    }
+}
