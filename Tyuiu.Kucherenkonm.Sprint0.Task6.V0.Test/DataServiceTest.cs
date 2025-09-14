bd896e71ca9e6add099ffd6bdc6c6932dec88da9
wyrtnvll <date1818@yandex.ru>
2025-09-14 23:21:51 +0500

    Task6

---
+++ Tyuiu.Kucherenkonm.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.Kucherenkonm.Sprint0.Task6.V0.Lib;
+namespace Tyuiu.Kucherenkonm.Sprint0.Task6.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.AdditionArray(numbers);
+            Assert.AreEqual(15, res);
+        }
+
+        [TestMethod]
+        public void CheckSubtractionValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.SubtractionArray(numbers);
+            Assert.AreEqual(-15, res);
+        }
+
+        [TestMethod]
+        public void CheckMultiplicationValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.MultiplicationArray(numbers);
+            Assert.AreEqual(120, res);
+        }
+    }
+}
